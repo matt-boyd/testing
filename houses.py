754f296b0d41e4bf37ccf2cd0b86053a5f209018
matt-boyd <matthewboyd123@gmail.com>
2017-08-31 21:58:39 +0100

    House script

---
+++ houses.py
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+import feedparser
+
+url = feedparser.parse('https://www.propertynews.com/rss.php?s=422487314')
+for houses in url['items']:
+	title =  houses['title'].encode('utf-8')
+	link =  houses['link'].encode('utf-8')
+	print title + "\n" +  link + "\n" + "\n" 
+
